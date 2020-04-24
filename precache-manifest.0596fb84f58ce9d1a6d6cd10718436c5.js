facefde1df40cbc22d56e5ca366c7b62868c03c0
WiltonHotz <v.horwing@gmail.com>
2020-04-24 18:00:54 +0200

    Updates

---
+++ precache-manifest.0596fb84f58ce9d1a6d6cd10718436c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499ba76b5fbc1d5fa7f984f2382263d3",
+    "url": "/React-monsters_rolodex/index.html"
+  },
+  {
+    "revision": "de65e37293c621d0d93c",
+    "url": "/React-monsters_rolodex/static/css/main.6cc3fea2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/React-monsters_rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-monsters_rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de65e37293c621d0d93c",
+    "url": "/React-monsters_rolodex/static/js/main.dd5d17e8.chunk.js"
+  },
+  {
+    "revision": "1ad46708c8010934fb9b",
+    "url": "/React-monsters_rolodex/static/js/runtime-main.306df3cc.js"
+  }
+]);
